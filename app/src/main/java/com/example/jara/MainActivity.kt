205c6c1fba5ffac5d46af140bb6dd129f11c45de
Pillsandelite <ezeadannalyn@gmail.com>
2021-08-23 20:21:30 +0100

    jara app

--- app/src/main/java/com/example/jara/MainActivity.kt
+++ app/src/main/java/com/example/jara/MainActivity.kt
@@ -5,19 +5,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image : ImageView = findViewById(R.id.imageView)
-        val clickButton : Button = findViewById(R.id.button)
+        val image: ImageView = findViewById(R.id.imageView)
+        val clickButton: Button = findViewById(R.id.button)
+        val background: ConstraintLayout = findViewById(R.id.background)
 
         clickButton.setOnClickListener {
             clickButton.text = getString(R.string.app_name)
             image.visibility = View.VISIBLE
-        }
 
+        background.setBackgroundColor(resources.getColor(R.color.black))
+    }}
 
-}}
+}
